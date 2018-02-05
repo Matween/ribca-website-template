ffadfe8d44feb213d31a344792414ba47b993a45
ririn <bozicmatic.bm@gmail.com>
2018-02-05 11:35:47 +0100

    on menu image click, some html elements now hide

--- assets/js/fullscreen.js
+++ assets/js/fullscreen.js
@@ -1,8 +1,19 @@
 var modal = $("#fullscreen");
 var img = $(".menu-img");
-var fullscreenImg = $("#fullscreen-img")
+var fullscreenImg = $("#fullscreen-img");
+
+var navContainer = $(".nav-container");
+var menu = $("#menu");
+var footer = $("#bottom");
+
+function toggleHTML() {
+    navContainer.toggle();
+    menu.toggle();
+    footer.toggle();
+}
 
 function openModal(source) {
+    toggleHTML();
     modal.css("display", "block");
     fullscreenImg.attr("src", source)
 }
@@ -15,4 +26,5 @@ img.each(function() {
 
 modal.find(".close").click(function() {
     modal.css("display", "none");
+    toggleHTML();
 });
